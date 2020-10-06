a1d61e4a84380d470c0aff593a5395faf46ef409
sharyu raut <sharyukraut210298@gmail.com>
2020-10-06 10:06:09 +0530

    [Sharyu] Add. uc3 Validating EmailId

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace UserRegistration
 {
@@ -6,7 +7,86 @@ namespace UserRegistration
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to User Validation Program!");
+            Console.WriteLine("Welcome to User Registration Program!");
+
+            Boolean validate = true;
+            int flag = 0;
+            Program check = new Program();
+
+            while (validate)
+            {
+                if (flag == 0)
+                    Console.WriteLine("Enter your first name: ");
+                else
+                    Console.WriteLine("Enter your first name again: ");
+
+                flag = 1;
+                string FirstName = Console.ReadLine();
+
+                Boolean Result = check.validateName(FirstName);
+                if (Result)
+                    validate = false;
+                else
+                    Console.WriteLine("Not a valid first name.");
+            }
+            validate = true;
+            flag = 0;
+
+
+            while (validate)
+            {
+                if (flag == 0)
+                    Console.WriteLine("Enter your last name: ");
+                else
+                    Console.WriteLine("Enter your last name again: ");
+
+                flag = 1;
+                string LastName = Console.ReadLine();
+
+                Boolean Result = check.validateName(LastName);
+                if (Result)
+                    validate = false;
+                else
+                    Console.WriteLine("Not a valid last name.");
+            }
+            validate = true;
+            flag = 0;
+
+            while (validate)
+            {
+                if (flag == 0)
+                    Console.WriteLine("Enter your EmailId: ");
+                else
+                    Console.WriteLine("Enter your EmailId again: ");
+
+                flag = 1;
+                string EmailId = Console.ReadLine();
+
+                Boolean Result = check.validateEmail(EmailId);
+                if (Result)
+                    validate = false;
+                else
+                    Console.WriteLine("Not a valid last name.");
+            }
+        }
+
+        private bool validateEmail(string emailId)
+        {
+            String EmailPattern = @"^[a-z][a-z0-9]+(\.[a-z0-9]+)?@[a-z0-9]+\.[a-z]{2,}(\.[a-z]{2,})?$";
+            Regex Pattern = new Regex(EmailPattern);
+
+            Boolean result = Pattern.IsMatch(emailId);
+            return result;
+        }
+
+        private Boolean validateName(String name)
+        {
+            String namePattern = @"^[A-Z]{1}[a-z]{2,}$";
+            Regex Pattern = new Regex(namePattern);
+
+            Boolean result = Pattern.IsMatch(name);
+
+            return result;
         }
     }
 }
