61af3f028f9888839f6464c0f7e74504663d3194
sharyu raut <sharyukraut210298@gmail.com>
2020-10-13 08:49:59 +0530

    [Sharyu] Refactor. Change in program.cs

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -19,95 +19,130 @@ namespace UserRegistration
 
             while (validate)
             {
-                if (flag == 0)
-                    Console.WriteLine("Enter your first name: ");
-                else
-                    Console.WriteLine("Enter your first name again: ");
+                try
+                {
+                    if (flag == 0)
+                        Console.WriteLine("Enter your first name: ");
+                    else
+                        Console.WriteLine("Enter your first name again: ");
 
-                flag = 1;
-                string FirstName = Console.ReadLine();
+                    flag = 1;
+                    string FirstName = Console.ReadLine();
 
-                bool Result = check.validateField(FirstName, namePattern);
-                if (Result)
-                    validate = false;
-                else
-                    Console.WriteLine("Not a valid first name.");
+                    bool Result = check.validateField(FirstName, namePattern);
+                    if (Result)
+                        validate = false;
+                    else
+                        Console.WriteLine("Not a valid first name.");
+                }
+                catch (UserRegistrationCustomException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
             }
             validate = true;
             flag = 0;
 
             while (validate)
             {
-                if (flag == 0)
-                    Console.WriteLine("Enter your last name: ");
-                else
-                    Console.WriteLine("Enter your last name again: ");
+                try
+                {
+                    if (flag == 0)
+                        Console.WriteLine("Enter your last name: ");
+                    else
+                        Console.WriteLine("Enter your last name again: ");
 
-                flag = 1;
-                string LastName = Console.ReadLine();
+                    flag = 1;
+                    string LastName = Console.ReadLine();
 
-                bool Result = check.validateField(LastName, namePattern);
-                if (Result)
-                    validate = false;
-                else
-                    Console.WriteLine("Not a valid last name.");
+                    bool Result = check.validateField(LastName, namePattern);
+                    if (Result)
+                        validate = false;
+                    else
+                        Console.WriteLine("Not a valid last name.");
+                }
+                catch (UserRegistrationCustomException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
             }
             validate = true;
             flag = 0;
 
             while (validate)
             {
-                if (flag == 0)
-                    Console.WriteLine("Enter your EmailId: ");
-                else
-                    Console.WriteLine("Enter your EmailId again: ");
+                try
+                {
+                    if (flag == 0)
+                        Console.WriteLine("Enter your EmailId: ");
+                    else
+                        Console.WriteLine("Enter your EmailId again: ");
 
-                flag = 1;
-                string EmailId = Console.ReadLine();
+                    flag = 1;
+                    string EmailId = Console.ReadLine();
 
-                bool Result = check.validateField(EmailId, EmailPattern);
-                if (Result)
-                    validate = false;
-                else
-                    Console.WriteLine("Not a valid last name.");
+                    bool Result = check.validateField(EmailId, EmailPattern);
+                    if (Result)
+                        validate = false;
+                    else
+                        Console.WriteLine("Not a valid last name.");
+                }
+                catch (UserRegistrationCustomException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
             }
             validate = true;
             flag = 0;
 
             while (validate)
             {
-                if (flag == 0)
-                    Console.WriteLine("Enter your Mobile number: ");
-                else
-                    Console.WriteLine("Enter your Mobile number again: ");
+                try
+                {
+                    if (flag == 0)
+                        Console.WriteLine("Enter your Mobile number: ");
+                    else
+                        Console.WriteLine("Enter your Mobile number again: ");
 
-                flag = 1;
-                string MobileNumber = Console.ReadLine();
+                    flag = 1;
+                    string MobileNumber = Console.ReadLine();
 
-                bool Result = check.validateField(MobileNumber, MobilePattern);
-                if (Result)
-                    validate = false;
-                else
-                    Console.WriteLine("Not a valid Mobile number.");
+                    bool Result = check.validateField(MobileNumber, MobilePattern);
+                    if (Result)
+                        validate = false;
+                    else
+                        Console.WriteLine("Not a valid Mobile number.");
+                }
+                catch (UserRegistrationCustomException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
             }
             validate = true;
             flag = 0;
 
             while (validate)
             {
-                if (flag == 0)
-                    Console.WriteLine("Enter your Password: ");
-                else
-                    Console.WriteLine("Enter your Password again: ");
+                try
+                {
+                    if (flag == 0)
+                        Console.WriteLine("Enter your Password: ");
+                    else
+                        Console.WriteLine("Enter your Password again: ");
 
-                flag = 1;
-                string Password = Console.ReadLine();
+                    flag = 1;
+                    string Password = Console.ReadLine();
 
-                bool Result = check.validateField(Password, passwordPattern);
-                if (Result)
-                    validate = false;
-                else
-                    Console.WriteLine("Not a valid Password.");
+                    bool Result = check.validateField(Password, passwordPattern);
+                    if (Result)
+                        validate = false;
+                    else
+                        Console.WriteLine("Not a valid Password.");
+                }
+                catch (UserRegistrationCustomException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
             }
 
             Console.WriteLine("Registration Successful!!!");
